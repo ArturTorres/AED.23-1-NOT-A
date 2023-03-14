ff1d3eb94c0e282d3b919579eddf1dd96a25e027
Marcel Hugo <37884743+marcelhugo70@users.noreply.github.com>
2023-03-14 20:35:47 -0300

    Lista 2 de exercícios

--- lineares/ListaEncadeada.java
+++ lineares/ListaEncadeada.java
@@ -38,27 +38,56 @@ public class ListaEncadeada implements Lista {
 	}
 
 	@Override
-	public void retirar(int valor) {
-		// TODO Auto-generated method stub
-
+	public void retirar(int valor) { // contribuição Elaine
+		NoLista anterior = null;
+		NoLista p = primeiro;
+		for (int i = 0; i < this.getTamanho(); i++) {
+			if (p.getInfo() == valor) {
+				break; // sair do laço
+			}
+			anterior = p;
+			p = p.getProx();
+		}
+		if (p != null) {
+			if (p == primeiro) {
+				this.primeiro = p.getProx();
+			} else {
+				anterior.setProx(p.getProx());
+			}
+			if (p == ultimo) {
+				this.ultimo = anterior;
+			}
+			this.qtdeElementos--;
+		}
 	}
 
 	@Override
-	public String exibir() {
-		// TODO Auto-generated method stub
-		return null;
+	public String exibir() { // contribuição Eduardo Lyra
+		String str = "[";
+		NoLista p = primeiro;
+		while (p != null) {
+			str += p.getInfo() + ", ";
+			p = p.getProx();
+		}
+		return str + "]";
 	}
 
 	@Override
-	public Lista copiar() {
-		// TODO Auto-generated method stub
-		return null;
+	public Lista copiar() { // contribuição Guilherme Milani
+		NoLista atual = primeiro;
+		Lista lista = new ListaEncadeada();
+		for (int i = 0; i < this.qtdeElementos; i++) {
+			lista.inserir(atual.getInfo());
+			atual = atual.getProx();
+		}
+		return lista;
 	}
 
 	@Override
 	public void concatenar(Lista outra) {
-		// TODO Auto-generated method stub
-
+		for (int i = 0; i < outra.getTamanho(); i++) {
+			this.inserir(outra.pegar(i));
+		}
 	}
 
 	@Override
@@ -67,15 +96,38 @@ public class ListaEncadeada implements Lista {
 	}
 
 	@Override
-	public int pegar(int pos) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int pegar(int pos) { // Contribuição Adriel
+		if (pos < 0 || pos >= this.qtdeElementos) {
+			throw new IndexOutOfBoundsException("Pos=" + pos);
+		}
+		NoLista p = primeiro;
+		int cont = 0;
+		while (p != null) {
+			if (cont == pos) {
+				return p.getInfo();
+			}
+			cont++;
+			p = p.getProx();
+		}
+		return -1; // nunca vai chegar aqui
 	}
 
 	@Override
-	public Lista dividir() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	public Lista dividir() {  // contribuição do Gustavo G
+		  int metade = getTamanho()/2;
+		  ListaEncadeada novaLista = new ListaEncadeada();
+		  NoLista no = primeiro;
+		  for (int i = 1; i < getTamanho(); i++) {
+		    if (i < metade) {
+		      no = no.getProx();
+		    } else {
+		      novaLista.inserir(pegar(i)); // observar a ordem de complexidade
+		    }
+		  }
+		  no.setProx(null);
+		  ultimo = no;
+		  qtdeElementos = metade;
+		  return novaLista;
+		}
 
 }
